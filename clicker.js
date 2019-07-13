dfc8aa0d642316f82f3a958321fa0be96fbb30d3
KoTuK3 <anton.menchinskii@gmail.com>
2019-07-13 12:49:05 +0300

    Переделал progress bar через div

--- clicker.js
+++ clicker.js
@@ -6,86 +6,89 @@ let priceForTenClick = 100;
 let priceForOneClickSec = 20;
 let priceForTenClickSec = 200;
 let experiencePoints = 0;
-let max = 100
-let level = 0
+let max = 100;
+let level = 0;
+let percent = 0;
+
 function checkExperiencePoints() {
-    if (experiencePoints >= max) {
-        experiencePoints = 0;
-        max *= 3;
-        level++
-        lvl.innerHTML = "Level: " + level;
-        document.getElementById('experience').setAttribute('value', experiencePoints);
-        document.getElementById('experience').setAttribute('max', max);
-    }
+  if (experiencePoints >= max) {
+    experiencePoints = 0;
+    max *= 3;
+    level++;
+    percent = 0;
+    lvl.innerHTML = "Level: " + level;
+    barValue.style.width = percent + "%";
+  }
 }
-document.getElementById('experience').setAttribute('max', max);
-const timerId = setInterval(function() {
-    scoreClicks += timerBonus;
-    score.innerHTML = "Score: " + scoreClicks;
-    experiencePoints += timerBonus;
-    document.getElementById('experience').setAttribute('value', experiencePoints);
-    checkExperiencePoints();
+
+const timerId = setInterval(function () {
+  scoreClicks += timerBonus;
+  score.innerHTML = "Score: " + scoreClicks;
+  experiencePoints += timerBonus;
+  percent = experiencePoints / (max / 100);
+  barValue.style.width = percent + "%"
+  checkExperiencePoints();
 }, 1000);
 
-document.getElementById('button').onclick = function() {
-    scoreClicks += bonusClicks;
-    experiencePoints += bonusClicks;
-    document.getElementById('experience').setAttribute('value', experiencePoints);
-    score.innerHTML = "Score: " + scoreClicks;
-    checkExperiencePoints();
-}
-document.getElementById('plusOne').onclick = function() {
-    if (scoreClicks >= priceForOneClick) {
+document.getElementById("button").addEventListener("click", function () {
+  scoreClicks += bonusClicks;
+  score.innerHTML = "Score: " + scoreClicks;
+  experiencePoints += bonusClicks;
+  percent = experiencePoints / (max / 100);
+  barValue.style.width = percent + "%"
+  checkExperiencePoints();
+});
+document.getElementById("plusOne").addEventListener("click", function () {
+  if (scoreClicks >= priceForOneClick) {
     bonusClicks++;
     scoreClicks -= priceForOneClick;
-    priceForOneClick = Math.floor(priceForOneClick *= 1.2);
+    priceForOneClick = Math.floor((priceForOneClick *= 1.2));
     click.innerHTML = "Click power: " + bonusClicks;
     score.innerHTML = "Score: " + scoreClicks;
     pricePlusOne.innerHTML = "Price: " + priceForOneClick;
-    } else return
-}
+  } else return;
+});
 
-document.getElementById('plusTen').onclick = function() {
-    if (scoreClicks >= priceForTenClick) {
+document.getElementById("plusTen").addEventListener("click", function () {
+  if (scoreClicks >= priceForTenClick) {
     bonusClicks += 10;
     scoreClicks -= priceForTenClick;
-    priceForTenClick = Math.floor(priceForTenClick *= 1.2);
-    click.innerHTML = "Click power: " + bonusClicks;    
+    priceForTenClick = Math.floor((priceForTenClick *= 1.2));
+    click.innerHTML = "Click power: " + bonusClicks;
     score.innerHTML = "Score: " + scoreClicks;
     pricePlusTen.innerHTML = "Price: " + priceForTenClick;
-    } else return
-}
+  } else return;
+});
 
-// document.getElementById('test').onclick = function() {
+// document.getElementById('test').addEventListener("click", function() {
 //     scoreClicks += 1000000000 ;
 //     score.innerHTML = "Score: " + scoreClicks;
-// }
+// });
 
-document.getElementById('plusOnePerSecond').onclick = function() {
-    if (scoreClicks >= priceForOneClickSec) {
-    scoreClicks -=priceForOneClickSec;
+document.getElementById("plusOnePerSecond").addEventListener("click", function () {
+  if (scoreClicks >= priceForOneClickSec) {
+    scoreClicks -= priceForOneClickSec;
     timerBonus += 1;
     priceForOneClickSec = Math.floor(priceForOneClickSec * 1.5);
     score.innerHTML = "Score: " + scoreClicks;
     clicksPerSecond.innerHTML = "Clicks per second: " + timerBonus;
     pricePlusOnePerSecond.innerHTML = "Price: " + priceForOneClickSec;
-    } else return
-}
+  } else return;
+});
 
-document.getElementById('plusTenPerSecond').onclick = function() {
-    if (scoreClicks >= priceForTenClickSec) {
-        scoreClicks -=priceForTenClickSec;
-        timerBonus += 10;
-        priceForTenClickSec = Math.floor(priceForTenClickSec * 1.5);
-        score.innerHTML = "Score: " + scoreClicks;
-        clicksPerSecond.innerHTML = "Clicks per second: " + timerBonus;
-        pricePlusTenPerSecond.innerHTML = "Price: " + priceForTenClickSec;
-    } else return
-}
+document.getElementById("plusTenPerSecond").addEventListener("click", function () {
+  if (scoreClicks >= priceForTenClickSec) {
+    scoreClicks -= priceForTenClickSec;
+    timerBonus += 10;
+    priceForTenClickSec = Math.floor(priceForTenClickSec * 1.5);
+    score.innerHTML = "Score: " + scoreClicks;
+    clicksPerSecond.innerHTML = "Clicks per second: " + timerBonus;
+    pricePlusTenPerSecond.innerHTML = "Price: " + priceForTenClickSec;
+  } else return;
+});
 
-// document.getElementById('testPerSecond').onclick = function() {
+// document.getElementById('testPerSecond').addEventListener("click", function() {
 //     timerBonus += 1000;
 //     score.innerHTML = "Score: " + scoreClicks;
 //     clicksPerSecond.innerHTML = "Clicks per second: " + timerBonus;
-// }
-
+// });
